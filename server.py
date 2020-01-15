6c37acfa070c15700b8c46b5a57f78c0b256ee70
Raz <razzo.des@gmail.com>
2020-01-15 19:40:39 +0200

    add: create student all courses and skills

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, render_template, redirect
 import time
-from data.data import students
-
+import json
+from data.student_data import students
+from data.school_db import courses_db, skills_db
 app = Flask(__name__)
 
 
@@ -17,7 +18,9 @@ def getstudents():
 
 @app.route('/new_student')
 def new_student():
-    return render_template('new_student.html')
+    course_list = []
+    skill_list = []
+    return render_template('new_student.html', courses=courses_db, skills=skills_db)
 
 
 @app.route('/new_student/add', methods=["POST"])
@@ -27,9 +30,21 @@ def add_student():
     student['last_name'] = request.form.get('last_name')
     student['time_created'] = time.time()
     student['time_updated'] = time.time()
+    student['id'] = f"ID:{student['first_name']}{int(time.time())}"
+    for course in courses_db:
+        student[course] = request.form.get(course.replace(" ", ""))
+    for skill in skills_db:
+        student[skill] = request.form.get(skill.replace(" ", ""))
     students.append(student)
-    print('add_student():', students)
+    print('add_student():', student)
     return redirect('http://127.0.0.1:5000/students')
+
+
+@app.route('/student_profile/<id>')
+def get_student_profile(id):
+    for student in students:
+        if student['id'] == id:
+            return render_template('student_profile.html', student=student)
 
 
 if __name__ == "__main__":
