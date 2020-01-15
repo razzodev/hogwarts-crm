b411b1836ec0f280f72b6850507e703f21c3a7cc
Raz <razzo.des@gmail.com>
2020-01-15 19:41:20 +0200

    refactor: enum to list

---
+++ data/school_db.py
@@ -0,0 +1,41 @@
+# from enum import Enum
+
+
+courses_db = ['Alchemy Basic', 'Alchemy Advanced',
+              'Magic for Everyone', 'Magic for THerapists', 'Magical Dating',
+              ]
+
+skills_db = ['Alchemy', 'Animation', 'Conjuror',
+             'Disintegration', 'Elemental', 'Healing', 'Illusion', 'Immortality',
+             'Invisibility', 'Invulnerability', 'Necromancer', 'Omnipresent',
+             'Omniscient', 'Poison', 'Possession', 'Self Detonation', 'Summoning',
+             'Water Breathing',
+             ]
+
+# class Courses(Enum):
+#     Alchemy_Basic = 1
+#     Alchemy_Advanced = 2
+#     Magic_For_Everyone = 3
+#     Magic_For_Therapists = 4
+#     Magical_Dating = 5
+
+
+# class Skills(Enum):
+#     Alchemy = 1
+#     Animation = 2
+#     Conjuror = 3
+#     Disintegration = 4
+#     Elemental = 5
+#     Healing = 6
+#     Illusion = 7
+#     Immortality = 8
+#     Invisibility = 9
+#     Invulnerability = 10
+#     Necromancer = 11
+#     Omnipresent = 12
+#     Omniscient = 13
+#     Poison = 14
+#     Possession = 15
+#     Self_Detonation = 16
+#     Summoning = 17
+#     Water_Breathing = 18
